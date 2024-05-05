72aecc5428ea2280ee77205cf7b7db3248e8019c
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-05-05 23:57:14 +0400

    Cetinfi

---
+++ AdoNET/AdoNET/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AdoNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            surnameInput = new TextBox();
+            nameInput = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            deleteBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Location = new Point(600, 16);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 48);
+            button2.TabIndex = 5;
+            button2.Text = "Add";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(14, 100);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(887, 484);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // surnameInput
+            // 
+            surnameInput.Location = new Point(238, 27);
+            surnameInput.Margin = new Padding(3, 4, 3, 4);
+            surnameInput.Name = "surnameInput";
+            surnameInput.Size = new Size(185, 27);
+            surnameInput.TabIndex = 2;
+            // 
+            // nameInput
+            // 
+            nameInput.Location = new Point(14, 27);
+            nameInput.Margin = new Padding(3, 4, 3, 4);
+            nameInput.Name = "nameInput";
+            nameInput.Size = new Size(157, 27);
+            nameInput.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(528, 28);
+            radioButton1.Margin = new Padding(3, 4, 3, 4);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(70, 24);
+            radioButton1.TabIndex = 4;
+            radioButton1.Text = "Qadın";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Checked = true;
+            radioButton2.Location = new Point(472, 28);
+            radioButton2.Margin = new Padding(3, 4, 3, 4);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(53, 24);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Kişi";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // deleteBtn
+            // 
+            deleteBtn.Location = new Point(759, 16);
+            deleteBtn.Margin = new Padding(3, 4, 3, 4);
+            deleteBtn.Name = "deleteBtn";
+            deleteBtn.Size = new Size(142, 48);
+            deleteBtn.TabIndex = 6;
+            deleteBtn.Text = "Delete";
+            deleteBtn.UseVisualStyleBackColor = true;
+            deleteBtn.Click += deleteBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(deleteBtn);
+            Controls.Add(radioButton2);
+            Controls.Add(nameInput);
+            Controls.Add(surnameInput);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(radioButton1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private DataGridView dataGridView1;
+        private TextBox surnameInput;
+        private TextBox nameInput;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button deleteBtn;
+    }
+}
